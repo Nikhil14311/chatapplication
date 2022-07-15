04071fbaf3cb4835af067d6c127696587badb461
Nikhil14311 <karanamnikhil00@gmail.com>
2022-07-14 22:29:38 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        // allowedHeaders: ["my-custom-header"],
+        // credentials: true
+        // methods : ["GET", "POST"],
+        // allowedHeaders : ["my-custom-header"],
+        // Credentials : true
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log('what is socket : ', socket);
+    console.log('Socket is active to be connected');
+
+    socket.on("chat", (payload) => {
+        console.log("what is payload", payload);
+        io.emit("chat", payload)
+    })
+})
+
+// app.listen(5000, () => console.log("server is active...."));
+server.listen(5000, () => console.log("server is active...."));
